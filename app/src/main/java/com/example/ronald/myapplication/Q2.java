d0f620fa5a44644c4d481780834c21df7dedff4d
Ronald Das <ronald1das@gmail.com>
2017-09-13 09:57:28 +0530

    Completed q1 and q2

--- app/src/main/java/com/example/ronald/myapplication/Q2.java
+++ app/src/main/java/com/example/ronald/myapplication/Q2.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ToggleButton;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Q2 extends AppCompatActivity {
@@ -20,14 +22,21 @@ public class Q2 extends AppCompatActivity {
         final EditText e1=(EditText)findViewById(R.id.q2_text1);
 
         final Button button1 = (Button)findViewById(R.id.q2_button1);
+        final ToggleButton tbutton1 = (ToggleButton)findViewById(R.id.q2_tbutton1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-
-                String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+                String date = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date());
 
                 e1.setText(date);
+                int secs;
+                secs = Calendar.getInstance().get(Calendar.SECOND);
+
+                if (secs <= 30)
+                    tbutton1.setChecked(true);
+                else
+                    tbutton1.setChecked(false);
 
             }
         });
