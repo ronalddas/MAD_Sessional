18dfa70a0eb3b704738a8b4a3b6248570a848d8a
Ronald Das <ronald1das@gmail.com>
2017-09-13 10:19:52 +0530

    Q4 is buggy

--- app/src/main/java/com/example/ronald/myapplication/Q4.java
+++ app/src/main/java/com/example/ronald/myapplication/Q4.java
@@ -2,6 +2,11 @@ package com.example.ronald.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Q4 extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class Q4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q4);
+
+        final EditText q4et1=(EditText)findViewById(R.id.q4et1);
+        final EditText q4et2=(EditText)findViewById(R.id.q4et2);
+        final EditText q4et3=(EditText)findViewById(R.id.q4et3);
+        final EditText q4et4=(EditText)findViewById(R.id.q4et4);
+
+        ArrayAdapter<String> state_adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,states);
+        state_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setAdapter(state_adapter);
+        findViewById(R.id.q4b).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt = q4et1.getText().toString()+" "+q4et2.getText().toString()+" "+q4et3.getText().toString()+" "+q4et4.getText().toString()+" "+spinner.getSelectedItem().toString();
+                Toast.makeText(MainActivity.this, txt, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
